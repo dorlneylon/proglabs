a14b56190386f9358f0cb35bc3910a48e5050386
Dorl Neylon <65495639+dorlneylon@users.noreply.github.com>
2022-10-01 17:50:29 +0300

    Add files via upload

--- lab2/main/moves/RazorLeaf.java
+++ lab2/main/moves/RazorLeaf.java
@@ -1,9 +1,11 @@
 package main.moves;
+
 import ru.ifmo.se.pokemon.*;
 
 public class RazorLeaf extends PhysicalMove {
     public RazorLeaf() {
         super(Type.GRASS, 55.0, 0.95);
+
     }
     
     @Override
