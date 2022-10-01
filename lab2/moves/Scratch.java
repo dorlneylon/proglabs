54504f2fb52b301a5d11cab75534961653c05f78
Dorl Neylon <65495639+dorlneylon@users.noreply.github.com>
2022-10-01 17:49:21 +0300

    Delete lab2/moves directory

--- lab2/moves/Scratch.java
+++
@@ -1,9 +0,0 @@
-package moves;
-import ru.ifmo.se.pokemon.*;
-
-public class Scratch extends PhysicalMove {
-    public Scratch() { super(Type.NORMAL, 40.0, 1.0); }
-
-    @Override
-    protected String describe() { return "использует Scratch"; }
-}
