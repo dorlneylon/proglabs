338cfdd03f5f490ff66b07d713477494099dae2c
Dorl Neylon <65495639+dorlneylon@users.noreply.github.com>
2022-10-08 14:02:56 +0300

    a

--- lab2/Main.java
+++ lab2/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Battle b = new Battle();
         Pokemon p1 = new Zangoose("Billy Harrington", 20);
-        Pokemon p2 = new Clauncher("Fucking slave", 5);
+        Pokemon p2 = new Clauncher("Slave", 5);
         Pokemon p3 = new Clawitzer("300$", 10);
         Pokemon p4 = new Bellsprout("BoyNextDoor", 2);
         Pokemon p5 = new Weepinbell("Van Darkholme", 10);
